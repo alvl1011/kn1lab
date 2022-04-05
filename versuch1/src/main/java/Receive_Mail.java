59743ee39225a0c6767a631542883e1c1d5fb367
alexandergrischancew <alexander.grischancew@yahoo.de>
2022-04-06 00:13:20 +0200

    Added info in case no messages were recieved

--- versuch1/src/main/java/Receive_Mail.java
+++ versuch1/src/main/java/Receive_Mail.java
@@ -16,7 +16,7 @@ public class Receive_Mail {
 			properties.put("mail.pop3.host", "localhost");
 			properties.put("mail.pop3.port", "110");
 
-			Session session = Session.getDefaultInstance(properties, null);
+			Session session = Session.getDefaultInstance(properties);
 
 			Store store = session.getStore("pop3");
 			store.connect("localhost", "labrat", "kn1lab");
@@ -27,13 +27,17 @@ public class Receive_Mail {
 			Message[] messages = folder.getMessages();
 			System.out.println("Messages:");
 			System.out.println(messages.length);
-			for (int i = 0, n = messages.length; i < n; i++){
-				Message message = messages[i];
-				System.out.println("Nr: " + (i+1));
-				System.out.println("Subject: " + message.getSubject());
-				System.out.println("From: " + message.getFrom()[0]);
-				System.out.println("Date: " + message.getSentDate());
-				System.out.println("Body: " + message.getContent().toString());
+			if (messages.length == 0){
+				System.out.println("No messages received");
+			} else {
+				for (int i = 0, n = messages.length; i < n; i++) {
+					Message message = messages[i];
+					System.out.println("Nr: " + (i + 1));
+					System.out.println("Subject: " + message.getSubject());
+					System.out.println("From: " + message.getFrom()[0]);
+					System.out.println("Date: " + message.getSentDate());
+					System.out.println("Body: " + message.getContent().toString());
+				}
 			}
 			folder.close();
 			store.close();
