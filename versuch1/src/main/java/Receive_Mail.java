ea00eed2f4756790805e868bd777c7e1b9723eba
alexandergrischancew <alexander.grischancew@yahoo.de>
2022-04-05 23:48:51 +0200

    Added functiionality

--- versuch1/src/main/java/Receive_Mail.java
+++ versuch1/src/main/java/Receive_Mail.java
@@ -12,6 +12,31 @@ public class Receive_Mail {
 	public static void fetchMail() {
 		try {
 			// your code here
+			Properties properties = new Properties();
+			properties.put("mail.pop3.host", "localhost");
+			properties.put("mail.pop3.port", "110");
+
+			Session session = Session.getDefaultInstance(properties, null);
+
+			Store store = session.getStore("pop3");
+			store.connect("localhost", "labrat", "kn1lab");
+
+			Folder folder = store.getFolder("INBOX");
+			folder.open(Folder.READ_ONLY);
+
+			Message[] messages = folder.getMessages();
+			System.out.println("Messages:");
+			System.out.println(messages.length);
+			for (int i = 0, n = messages.length; i < n; i++){
+				Message message = messages[i];
+				System.out.println("Nr: " + (i+1));
+				System.out.println("Subject: " + message.getSubject());
+				System.out.println("From: " + message.getFrom()[0]);
+				System.out.println("Date: " + message.getSentDate());
+				System.out.println("Body: " + message.getContent().toString());
+			}
+			folder.close();
+			store.close();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
